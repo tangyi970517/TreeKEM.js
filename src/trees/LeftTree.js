5937d3a0298c1c107d2161ed5d8652ff767ad26a
Tang, Yi <tangyi970517@gmail.com>
2024-02-20 13:11:17 -0500

    add balanced LBBT

--- src/trees/LeftTree.js
+++ src/trees/LeftTree.js
@@ -9,6 +9,7 @@ const LeftTreeEnums = {
 		'append',
 	],
 	truncate: [
+		'balance',
 		'truncate',
 		'keep',
 	],
@@ -85,6 +86,80 @@ class LeftTree extends BinaryTree {
 		return new this.constructor(epoch, [this.rawL, childNew], childNew);
 	}
 
+	* split(epoch, node) {
+		for (const sibling of node.getCopath(epoch)) {
+			yield sibling;
+			if (this.children.includes(sibling)) {
+				return;
+			}
+		}
+		assert(false);
+	}
+	static merge(epoch, roots) {
+		assert(roots.length > 0);
+		if (roots.length === 1) {
+			return roots[0];
+		}
+		const [childR, childL] = roots.slice(0, 2);
+		const childTrace = childR.epoch === epoch ? childR : null;
+		const node = new this(epoch, [childL, childR], childTrace);
+		roots.splice(0, 2, node); // mutate for efficiency
+		return this.merge(epoch, roots);
+	}
+	getRightPerfect() {
+		assert(!this.isPerfect);
+		if (this.rawR.isPerfect) {
+			return this.rawR;
+		}
+		return this.rawR.getRightPerfect();
+	}
+	pop(epoch, leaf) {
+		const path = [...leaf.getPath(epoch)];
+		const indexPerfect = path.findLastIndex(node => node.isPerfect);
+		const nodePerfect = path[indexPerfect];
+		const nodeImperfect = indexPerfect < path.length-1 ? path[indexPerfect+1] : null;
+		if (nodePerfect === leaf) {
+			if (nodeImperfect === null) {
+				assert(false, 'attempting to remove the last node');
+			}
+			assert(nodePerfect === nodeImperfect.rawR);
+			const parent = nodeImperfect.getParent(epoch);
+			if (parent === null) {
+				return nodeImperfect.rawL.setParent(epoch, null);
+			}
+			assert(nodeImperfect === parent.rawR);
+			const parentNew = new this.constructor(epoch, [parent.rawL, nodeImperfect.rawL], null);
+			return parent.replace(epoch, parentNew);
+		}
+		const split = [...nodePerfect.split(epoch, leaf)];
+		if (nodeImperfect === null || nodePerfect === nodeImperfect.rawR) {
+			if (split.length === 1) {
+				if (nodeImperfect === null) {
+					return split[0].setParent(epoch, null);
+				}
+				const nodeImperfectNew = new this.constructor(epoch, [nodeImperfect.rawL, split[0]], null);
+				return nodeImperfect.replace(epoch, nodeImperfectNew);
+			}
+			return nodePerfect.replace(epoch, this.constructor.merge(epoch, split));
+		}
+		const nodeRight = nodeImperfect.rawR;
+		const nodeRightPerfect = nodeImperfect.getRightPerfect();
+		if (nodeRightPerfect === nodeRight && nodeRightPerfect.height === 0) {
+			split.unshift(nodeRightPerfect); // mutate for efficiency
+			return nodeImperfect.replace(epoch, this.constructor.merge(epoch, split));
+		}
+		const splitRight = nodeRightPerfect !== nodeRight ? [...nodeRight.split(epoch, nodeRightPerfect)] : [];
+		const indexRightPerfect = split.findIndex(node => node.height === nodeRightPerfect.height);
+		assert(indexRightPerfect >= 0);
+		assert((indexRightPerfect === 0) === (nodeRightPerfect.height === 0));
+		const splitShift = split.splice(0, indexRightPerfect, nodeRightPerfect); // mutate for efficiency
+		const nodePerfectNew = this.constructor.merge(epoch, split);
+		splitRight.unshift(...splitShift); // mutate for efficiency
+		const nodeRightNew = this.constructor.merge(epoch, splitRight);
+		const nodeImperfectNew = new this.constructor(epoch, [nodePerfectNew, nodeRightNew], nodePerfectNew);
+		return nodeImperfect.replace(epoch, nodeImperfectNew);
+	}
+
 	truncate(epoch) {
 		if (this.isAllRemoved) {
 			return null;
@@ -123,9 +198,12 @@ class LeftTree extends BinaryTree {
 
 	remove(epoch, leaf, _hint) {
 		assert(leaf.isLeaf && leaf.getRoot(epoch) === this);
-		const rootNew = super.remove(epoch, leaf);
 		switch (truncate) {
+			case 'balance': {
+				return this.pop(epoch, leaf);
+			} break;
 			case 'truncate': {
+				const rootNew = super.remove(epoch, leaf);
 				const rootTruncate = rootNew.truncate(epoch);
 				assert(rootTruncate !== null, 'attempting to remove the last node');
 				if (rootTruncate.epoch < epoch) {
@@ -134,6 +212,7 @@ class LeftTree extends BinaryTree {
 				return rootTruncate;
 			} break;
 			case 'keep': {
+				const rootNew = super.remove(epoch, leaf);
 				return rootNew;
 			} break;
 		}
