2176b9697a477894e12b66d139b6fc34255fdf09
Tang, Yi <tangyi970517@gmail.com>
2024-03-03 14:14:06 -0500

    rewrite by taint manager

--- src/TreeKEM.js
+++ src/TreeKEM.js
@@ -2,7 +2,7 @@ import {assert, sum} from './utils.js';
 
 const isSkeleton = (node, epochOld, skeletonExtra) => !node.isAllRemoved && (node.epoch > epochOld || skeletonExtra.has(node));
 
-const recompose = (node, taintMap) => {
+const recompose = node => {
 	assert(!node.isLeaf);
 	if (!node.decompose) {
 		return;
@@ -15,42 +15,45 @@ const recompose = (node, taintMap) => {
 	if (nodeMain.isAllRemoved) {
 		return;
 	}
-	if (!nodeMain.data.pk) {
-		recompose(nodeMain, taintMap);
+	let nodeSecret;
+	if (nodeMain.data.pk) {
+		nodeSecret = nodeMain;
+	} else {
+		nodeSecret = recompose(nodeMain);
 	}
+	assert((!nodeMain.data.pk) === (!nodeSecret));
 	if (nodeMain.data.pk) {
 		node.data.pk = nodeMain.data.pk;
 		node.data.sk = nodeMain.data.sk;
 		node.data.kk = nodeMain.data.kk;
 		node.data.unmerged = [].concat(nodeMain.data.unmerged ?? [], node.decompose.slice(1));
-		if (!nodeMain.data.taintBy) {
-			return;
-		}
-		for (const leaf of nodeMain.data.taintBy) {
-			taintNode(taintMap, leaf, node);
-		}
-		rinseNode(taintMap, nodeMain);
 	}
+	return nodeSecret;
 };
 
-const taintNode = (taintMap, user, node) => {
-	if (!taintMap.has(user)) {
-		taintMap.set(user, new Set());
+class TaintManager {
+	constructor() {
+		this.taintMap = new WeakMap();
+		this.taintByMap = new WeakMap();
 	}
-	const taintSet = taintMap.get(user);
-	assert(!taintSet.has(node));
-	taintSet.add(node);
-	if (!node.data.taintBy) {
-		node.data.taintBy = new Set();
+
+	taint(user, node) {
+		const {taintMap, taintByMap} = this;
+		if (!taintMap.has(user)) {
+			taintMap.set(user, new Set());
+		}
+		const taintSet = taintMap.get(user);
+		assert(!taintSet.has(node));
+		taintSet.add(node);
+		if (!taintByMap.has(node)) {
+			taintByMap.set(node, new Set());
+		}
+		const taintBy = taintByMap.get(node);
+		assert(!taintBy.has(user));
+		taintBy.add(user);
 	}
-	assert(!node.data.taintBy.has(user));
-	node.data.taintBy.add(user);
-};
-const rinseNode = (taintMap, node) => {
-	if (!node.data.taintBy) {
-		return;
-	}
-	for (const user of node.data.taintBy) {
+	undoTaintForUser(user, node) {
+		const {taintMap} = this;
 		assert(taintMap.has(user));
 		const taintSet = taintMap.get(user);
 		assert(taintSet.has(node));
@@ -59,20 +62,87 @@ const rinseNode = (taintMap, node) => {
 			taintMap.delete(user);
 		}
 	}
-	node.data.taintBy = null;
-};
+	undoTaintForNode(user, node) {
+		const {taintByMap} = this;
+		assert(taintByMap.has(node));
+		const taintBy = taintByMap.get(node);
+		assert(taintBy.has(user));
+		taintBy.delete(user);
+		if (taintBy.size === 0) {
+			taintByMap.delete(node);
+		}
+	}
 
-const rinseTill = (taintMap, node, epochNew, rootNew) => {
-	if (node.getRoot(epochNew, true) === rootNew) {
-		return;
+	hasTaint(user, node) {
+		const {taintMap} = this;
+		if (!taintMap.has(user)) {
+			return false;
+		}
+		const taintSet = taintMap.get(user);
+		return taintSet.has(node);
 	}
-	for (const child of node.children) {
-		rinseTill(taintMap, child, epochNew, rootNew);
+	* getTaint(user) {
+		const {taintMap} = this;
+		if (!taintMap.has(user)) {
+			return;
+		}
+		yield * taintMap.get(user);
 	}
-	if (!node.isComponent) {
-		rinseNode(taintMap, node);
+
+	rinseNode(node) {
+		const {taintByMap} = this;
+		if (!taintByMap.has(node)) {
+			return;
+		}
+		for (const user of taintByMap.get(node)) {
+			this.undoTaintForUser(user, node);
+		}
+		taintByMap.delete(node);
 	}
-};
+
+	rinseTill(node, epochNew, rootNew) {
+		if (node.getRoot(epochNew, true) === rootNew) {
+			return;
+		}
+		for (const child of node.children) {
+			this.rinseTill(child, epochNew, rootNew);
+		}
+		if (!node.isComponent) {
+			this.rinseNode(node);
+		}
+	}
+
+	rinseUser(user) {
+		const {taintMap} = this;
+		if (!taintMap.has(user)) {
+			return;
+		}
+		for (const node of taintMap.get(user)) {
+			this.undoTaintForNode(user, node);
+		}
+		taintMap.delete(user);
+	}
+
+	replaceTaint(node, nodeNew) {
+		const {taintMap, taintByMap} = this;
+		if (!taintByMap.has(node)) {
+			return;
+		}
+		const taintBy = taintByMap.get(node);
+		taintByMap.delete(node);
+		if (!taintByMap.has(nodeNew)) {
+			taintByMap.set(nodeNew, taintBy);
+		}
+		for (const user of taintBy) {
+			assert(taintMap.has(user));
+			const taintSet = taintMap.get(user);
+			assert(taintSet.has(node));
+			taintSet.delete(node);
+			assert(!taintSet.has(nodeNew));
+			taintSet.add(nodeNew);
+		}
+	}
+}
 
 const skeletonIter = function * (root, epochOld, epoch, skeletonExtra, path, regionPredicate, trace = new Map()) {
 	assert(isSkeleton(root, epochOld, skeletonExtra));
@@ -161,7 +231,7 @@ class TreeKEM {
 
 		this.regionEnc = new RegionTypeEnc();
 		this.regionDec = new RegionTypeDec();
-		this.taint = new Map();
+		this.taint = new TaintManager();
 
 		this.skeletonProposal = new Set();
 		this.epochCommitted = 0;
@@ -195,7 +265,6 @@ class TreeKEM {
 		leaf.data.pk = pk;
 		leaf.data.sk = sk;
 		leaf.data.kk = null;
-		leaf.data.taintBy = null;
 		leaf.data.sizeBlank = 0;
 	}
 
@@ -217,7 +286,7 @@ class TreeKEM {
 			this.commit(a);
 		}
 
-		rinseTill(this.taint, treeOld, this.epoch, this.tree);
+		this.taint.rinseTill(treeOld, this.epoch, this.tree);
 		if (clearingOldNodes) {
 			treeOld.clearTill(this.epoch, this.tree);
 		}
@@ -238,26 +307,19 @@ class TreeKEM {
 		if (root.epoch < this.epoch) {
 			skeletonExtra.add(root);
 		}
-		if (this.taint.has(ub)) {
-			for (const node of this.taint.get(ub)) {
-				assert(node.data.taintBy?.has(ub));
-				node.data.taintBy.delete(ub);
-				if (node.data.taintBy.size === 0) {
-					node.data.taintBy = null;
-				}
-				if (node.getRoot(this.epoch, true) === this.tree) {
-					insertPath(this.epoch, node, skeletonExtra);
-				}
+		for (const node of this.taint.getTaint(ub)) {
+			if (node.getRoot(this.epoch, true) === this.tree) {
+				insertPath(this.epoch, node, skeletonExtra);
 			}
-			this.taint.delete(ub);
 		}
+		this.taint.rinseUser(ub);
 
 		this.insertSkeleton(skeletonExtra);
 		if (!usingProposal) {
 			this.commit(a);
 		}
 
-		rinseTill(this.taint, treeOld, this.epoch, this.tree);
+		this.taint.rinseTill(treeOld, this.epoch, this.tree);
 		if (clearingOldNodes) {
 			treeOld.clearTill(this.epoch, this.tree);
 		}
@@ -271,11 +333,11 @@ class TreeKEM {
 		const ub = this.users.get(b);
 
 		const skeletonExtra = new Set(ub.getPath(this.epoch));
-		if (this.taint.has(ub)) {
-			for (const node of this.taint.get(ub)) {
-				if (node.getRoot(this.epoch, true) === this.tree) {
-					insertPath(this.epoch, node, skeletonExtra);
-				}
+		for (const node of this.taint.getTaint(ub)) {
+			if (node.getRoot(this.epoch, true) === this.tree) {
+				insertPath(this.epoch, node, skeletonExtra);
+			} else {
+				assert(node.isComponent);
 			}
 		}
 
@@ -342,10 +404,11 @@ for (const _ of function * () {
 			node.data.sk = null;
 			node.data.kk = null;
 			node.data.unmerged = null;
-			rinseNode(this.taint, node);
+			this.taint.rinseNode(node);
 			if (!isInRegion) {
 				if (usingUnmergedNodesForBlank) {
-					recompose(node, this.taint);
+					const nodeSecret = recompose(node);
+					this.taint.replaceTaint(nodeSecret, node);
 					if (node.data.pk) {
 						--node.data.sizeBlank;
 					}
@@ -353,7 +416,8 @@ for (const _ of function * () {
 				continue;
 			}
 			if (usingUnmergedNodesForSecret) {
-				recompose(node, this.taint);
+				const nodeSecret = recompose(node);
+				this.taint.replaceTaint(nodeSecret, node);
 				if (node.data.pk) {
 					continue;
 				}
@@ -376,7 +440,7 @@ for (const _ of function * () {
 			seedMap.set(node, seedNext);
 			[node.data.pk, node.data.sk] = this.crypto.PKE_Gen(secret);
 			if (!path.has(node)) {
-				taintNode(this.taint, leaf, node);
+				this.taint.taint(leaf, node);
 			}
 			for (const child of node.children) {
 				if (child === childTrace) {
@@ -384,7 +448,7 @@ for (const _ of function * () {
 				}
 				yield * this.skeletonEnc(child, seed, leaf, path);
 			}
-			for (const [leafOther, _] of this.taint) {
+			for (const leafOther of [ /* hint from `regionDec` */ ]) {
 				if (leafOther === leaf) {
 					continue;
 				}
@@ -394,7 +458,7 @@ for (const _ of function * () {
 				}
 				assert(leafOther.data.pk);
 				this.crypto.PKE_Enc(leafOther.data.pk, seed);
-				taintNode(this.taint, leafOther, node);
+				this.taint.taint(leafOther, node);
 			}
 		}
 		assert(skeletonExtra.size === 0);
@@ -419,7 +483,7 @@ for (const _ of function * () {
 		assert(!root.isLeaf || !root.data.kk);
 		assert(!usingSKE || root.isLeaf || (!root.data.pk) === (!root.data.kk));
 		if (root.data.pk) {
-			if (root.data.kk && (path.has(root) || root.data.taintBy?.has(leaf))) {
+			if (root.data.kk && (path.has(root) || this.taint.hasTaint(leaf, root))) {
 				yield this.crypto.SKE_Enc(root.data.kk, seed);
 			} else {
 				yield this.crypto.PKE_Enc(root.data.pk, seed);
